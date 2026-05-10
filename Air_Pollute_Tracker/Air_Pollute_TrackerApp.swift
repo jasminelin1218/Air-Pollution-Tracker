5d1c1368471b40e866fa6a9bea572f0203366da2
Jasmine <jasminelin1218@gmail.com>
2026-05-09 18:33:45 -0700

    Initial Commit

---
+++ Air_Pollute_Tracker/Air_Pollute_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_Pollute_TrackerApp.swift
+//  Air_Pollute_Tracker
+//
+//  Created by Jasmine Lin on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Air_Pollute_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Air_Pollute_Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Air_Pollute_Tracker
+//
+//  Created by Jasmine Lin on 5/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
